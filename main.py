ba5e561cf0d36d54b2fe1360e0b48b2c8cdaa2b5
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-09-09 10:27:03 -0400

    sdfgdfhd

---
+++ main.py
@@ -0,0 +1,22 @@
+#Lucas Brinks
+#09/09/24
+#Print Variables
+
+# Exercise 2 - 3  (Personal Message)
+
+name = "Lucas Brinks"
+print(f"Hello {name}, would you like to learn some python today.")
+
+#  Exercise 2 - 5 (Famous Quote)
+
+quote = 'The only thing we have to fear is fear itself.'
+author = '-Franklin D. Roosevelt'
+full_quote = f'{quote} {author}'
+print (f'{full_quote}')
+
+# Exercise 2 - 6 (Famous Quote 2)
+
+quote2 = 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.'
+author2 = '-Dr. Seuss'
+full_quote2 = f'{quote2} {author2}'
+print(f'{full_quote2}')
